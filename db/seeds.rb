33d5f3606c98ea6cfbe6918c41ae97ec4c408aae
Simon Grchevski <simongrcevski@gmail.com>
2021-11-30 19:28:47 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  puts 'Start seeding'
+  %w[Hello Ola Zivjo Hola Zdravo].each { |g| Greeting.create(greet: g) }
+  puts 'End seeding'
